7844e9bbbadbc9bea5308b12429059276b84be87
ichigo-D-Uzumaki <mohammedarfath.meeer@gmail.com>
2026-04-14 23:06:54 +0530

    aaaaaaaa

--- api/data.js
+++ api/data.js
@@ -23,5 +23,21 @@ export default async function handler(req, res) {
             .insert([{ name, email }]);
 
         res.status(200).json(data);
+
+    }
+    if (req.method === "POST") {
+        const { name, email } = req.body;
+
+        const { data } = await supabase
+            .from('users')
+            .select('*')
+            .eq('name', name)
+            .eq('email', email);
+
+        if (data.length > 0) {
+            return res.status(200).json({ success: true });
+        } else {
+            return res.status(401).json({ success: false });
+        }
     }
 }
