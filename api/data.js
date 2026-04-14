83cf7cfb1fd6727acb37e8b0223bfb3562317384
ichigo-D-Uzumaki <mohammedarfath.meeer@gmail.com>
2026-04-14 22:49:14 +0530

    dtvsimv2

---
+++ api/data.js
@@ -0,0 +1,27 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+
+    if (req.method === "GET") {
+        const { data } = await supabase
+            .from('users')
+            .select('*');
+
+        res.status(200).json(data);
+    }
+
+    if (req.method === "POST") {
+        const { name, email } = req.body;
+
+        const { data } = await supabase
+            .from('users')
+            .insert([{ name, email }]);
+
+        res.status(200).json(data);
+    }
+}
